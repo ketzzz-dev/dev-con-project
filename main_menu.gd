f4b4ef9b61fb5f79447fcaeb5cd205b0221d7379
YamatoDev <bibaocomatthew@gmail.com>
2025-08-08 22:11:28 +0800

    Audio and Sound Effects

--- main_menu.gd
+++ main_menu.gd
@@ -1,5 +1,19 @@
 extends Control
 
+func _ready() -> void:
+	$Audio/BGM.play()
 
-func _on_button_pressed() -> void:
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_start_pressed() -> void:
+	_play_button_sound()
 	get_tree().change_scene_to_file("res://scenes/test.tscn")
+
+
+func _on_settings_pressed() -> void:
+	_play_button_sound()
+	$VBoxContainer.visible = false;
+
+func _play_button_sound():
+	$Audio/ButtonSFX.play()
