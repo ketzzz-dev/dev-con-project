17afd6892f611c50572b7a617b6a48fc54db16a8
YamatoDev <bibaocomatthew@gmail.com>
2025-08-08 23:04:39 +0800

    Added Audio Sliders

--- main_menu.gd
+++ main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready() -> void:
 	$Audio/BGM.play()
+	$AudioPanel.visible = false
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
@@ -10,10 +11,15 @@ func _on_start_pressed() -> void:
 	_play_button_sound()
 	get_tree().change_scene_to_file("res://scenes/test.tscn")
 
-
 func _on_settings_pressed() -> void:
 	_play_button_sound()
-	$VBoxContainer.visible = false;
+	$VBoxContainer.visible = false
+	$AudioPanel.visible = true
 
 func _play_button_sound():
-	$Audio/ButtonSFX.play()
+	$Audio/Button.play()
+
+func _on_leave_pressed() -> void:
+	_play_button_sound()
+	$VBoxContainer.visible = true
+	$AudioPanel.visible = false
