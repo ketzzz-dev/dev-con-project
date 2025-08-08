0624084fb4de997fbbd0751d36d445dc14e2bc9d
Keys <0hlowkey750@gmail.com>
2025-08-08 19:01:59 +0800

    Initial commit

---
+++ scripts/player/movement.gd
@@ -0,0 +1,34 @@
+extends Node3D
+
+@export var speed: float
+@export var acceleration: float
+@export var deceleration: float
+
+@onready var player := get_parent()
+
+var input_direction := Vector2.ZERO
+
+func _process(_delta: float) -> void:
+	input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+func _physics_process(delta: float) -> void:
+	if not player.is_on_floor():
+		player.velocity += player.get_gravity() * delta
+	else:
+		move(delta)
+	
+	player.move_and_slide()
+
+func move(delta: float) -> void:
+	var move_direction := Vector3(input_direction.x, 0, input_direction.y)
+	var is_moving := move_direction.length_squared() > 0
+	
+	if is_moving:
+		move_direction = move_direction.normalized()
+	
+	var target_velocity = move_direction * speed
+	var delta_velocity = target_velocity - player.velocity
+	var acceleration_rate = acceleration if is_moving else deceleration
+	
+	player.velocity.x += delta_velocity.x * acceleration_rate * delta
+	player.velocity.z += delta_velocity.z * acceleration_rate * delta
