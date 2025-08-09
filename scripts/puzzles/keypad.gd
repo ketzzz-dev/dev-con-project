08c906a819238d6a893bf04fd3553ef55d3e9e67
Pasta <55084584+Pasas1345@users.noreply.github.com>
2025-08-10 00:36:50 +0800

    KEYPAD WORKS

--- scripts/puzzles/keypad.gd
+++ scripts/puzzles/keypad.gd
@@ -1,7 +1,16 @@
 class_name Keypad extends Interactable
 
 @export var pass_code: String
-var opened
+@onready var pass_len: int = pass_code.length()
+
+var is_operating: bool = false
+
+var opened: bool
 
 func interact() -> void:
-	print("HELLO")
+	if (!is_operating && !opened):
+		print("starting ui")
+		get_node("../%KeypadUI").get_node("Panel").start_keypad_ui(self)
+
+func finish_typing() -> void:
+	pass
