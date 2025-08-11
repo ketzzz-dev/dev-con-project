06f8dfd93ffb03dc2de0508b75f5c98bee40d0dc
YamatoDev <bibaocomatthew@gmail.com>
2025-08-12 00:50:20 +0800

    Added Assets

--- scripts/entities/props/lever.gd
+++ scripts/entities/props/lever.gd
@@ -1,5 +1,8 @@
 class_name Lever extends Interactable
 
+@export var delayTime: float
 
 func interact() -> void:
+	$"../Sprite3D".texture = load("res://assets/Puzzle Sprites/Lever_active.png")
+	await get_tree().create_timer(delayTime).timeout
 	get_tree().change_scene_to_file("res://scenes/ui/screens/main_menu.tscn")
